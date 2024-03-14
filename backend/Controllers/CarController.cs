02f3e1041921816a21017251de551f5a1d8c8e66
assuy <43821373+assuy@users.noreply.github.com>
2024-03-14 20:08:36 +0100

    Add car and location controller

---
+++ backend/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("locations/{locationID}/cars")]
+    public class CarController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCarsInLocation(long locationID)
+        {
+            var location = await _context.Locations.FindAsync(locationID);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            var cars = await _context.Cars.Where(car => car.LocationId == locationID).ToListAsync();
+
+            return cars;
+        }
+    }
+}
---
+++ backend/Controllers/LocationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("locations")]
+    public class LocationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public LocationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Location>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        [HttpGet("{locationID}")]
+        public async Task<ActionResult<Location>> GetLocation(long locationID)
+        {
+            var location = await _context.Locations.FindAsync(locationID);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return location;
+        }
+    }
+}
