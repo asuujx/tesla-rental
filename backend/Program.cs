04dfb86c4a1230d63f9f2917dd17e780806c271b
assuy <43821373+assuy@users.noreply.github.com>
2024-03-14 20:27:57 +0100

    Set up CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,20 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Database connection
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbConnectionString");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 // Add controllers to the services
 builder.Services.AddControllers();
@@ -10,6 +22,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 // Routing and map controllers
 app.UseRouting();
