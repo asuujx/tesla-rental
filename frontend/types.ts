4ee7cc79d02d7a150d962635683b06176b0cfa7e
assuy <43821373+assuy@users.noreply.github.com>
2024-03-15 15:14:35 +0100

    Add type interfaces

--- frontend/types.ts
+++ frontend/types.ts
@@ -2,3 +2,30 @@ export interface Location {
    id: number;
    name: string;
 }
+
+export interface Car {
+   id: number;
+   name: string;
+   image: string;
+   price: number;
+   locationId: number;
+}
+
+export interface User {
+   id: number;
+   login: string;
+   password: string;
+   name: string;
+   surname: string;
+   email: string;
+   phone: string;
+}
+
+export interface Reservation {
+   id: number;
+   userId: number;
+   carId: number;
+   startDate: string;
+   endDate: string;
+   totalPrice: number;
+}
