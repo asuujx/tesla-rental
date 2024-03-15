d3e994ad424e7a41b264833af03c304088dd48f0
assuy <43821373+assuy@users.noreply.github.com>
2024-03-15 18:40:28 +0100

    Update user model

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -4,16 +4,9 @@ namespace backend.Models
     {
         public long Id { get; set; }
         [Required]
-        public string Login { get; set; }
-        [Required]
-        public string Password { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public string Surname { get; set; }
-        [Required]
         public string Email { get; set; }
         [Required]
-        public string Phone { get; set; }
+        public string Password { get; set; }
+
     }
 }
