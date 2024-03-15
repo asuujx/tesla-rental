ec87871df9415337a7f424a982fa631c248f59c9
assuy <43821373+assuy@users.noreply.github.com>
2024-03-15 15:11:49 +0100

    Add ImageController

---
+++ backend/Controllers/ImageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IHostEnvironment _env;
+        public ImageController(IHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet("{imageName}")]
+        public IActionResult GetImage(string imageName)
+        {
+            var imagePath = Path.Combine(_env.ContentRootPath, "Images", imageName);
+            if (!System.IO.File.Exists(imagePath))
+                return NotFound();
+
+            var image = System.IO.File.OpenRead(imagePath);
+            return File(image, "image/jpeg");
+        }
+    }
+}
