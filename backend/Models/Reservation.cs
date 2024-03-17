2622ed8ca7059c56e0b5e2ce58c515cb97fe21c1
assuy <43821373+assuy@users.noreply.github.com>
2024-03-18 00:18:40 +0100

    Update reservation model

--- backend/Models/Reservation.cs
+++ backend/Models/Reservation.cs
@@ -3,19 +3,18 @@ namespace backend.Models
     public class Reservation
     {
         public long Id { get; set; }
-        [Required]
-        public long UserId { get; set; }
-        [Required]
-        public long CarId { get; set; }
-        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string StartLocation { get; set; }
         public DateTime StartDate { get; set; }
-        [Required]
-        public DateTime EndDate { get; set; }
-        [Required]
+        public string EndLocation { get; set; }
+        public DateTime EndDate {  get; set; }
+        public long CarId { get; set; }
         public float TotalPrice { get; set; }
 
-        // Navigation properties
-        public User User { get; set; }
+        // Navigation property
         public Car Car { get; set; }
     }
 }
