e920d15273dfe21510aa7d1ccf352160ef30f27c
assuy <43821373+assuy@users.noreply.github.com>
2024-03-18 00:20:26 +0100

    Add reservation DTO

---
+++ backend/DTOs/ReservationDto.cs
@@ -0,0 +1,27 @@
+﻿namespace backend.DTOs
+{
+    public class ReservationDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string StartLocation { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public string EndLocation { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public long CarId { get; set; }
+        [Required]
+        public float TotalPrice { get; set; }
+    }
+}
