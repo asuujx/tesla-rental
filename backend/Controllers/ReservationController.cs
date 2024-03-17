12f72ad4cfa8aa1dd4ca92f9fc3645f3840dba4d
assuy <43821373+assuy@users.noreply.github.com>
2024-03-18 00:19:03 +0100

    Add reservation controller

---
+++ backend/Controllers/ReservationController.cs
@@ -0,0 +1,23 @@
+﻿using backend.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("/reservations")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        // TODO: Add an endpoint for getting all reservations
+
+        [HttpPost("create-reservation")]
+        public IActionResult CreateReservation([FromBody] ReservationDto data)
+        {
+            Console.WriteLine($"Received data: {System.Text.Json.JsonSerializer.Serialize(data)}");
+            // TODO: Implement reservation creation logic here
+
+            return Ok();
+        }
+    }
+}
+    
